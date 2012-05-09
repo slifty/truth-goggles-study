3a0850237de0cf3a8590bf91a94259de19b05558
Daniel Schultz <slifty@gmail.com>
2012-05-09 00:26:25 -0400

    better thank you page

--- study_thanks.php
+++ study_thanks.php
@@ -12,6 +12,11 @@
 		<link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" title="no title" charset="utf-8">
 	</head>
 	<body>
-		<div id="content">All Done!  Thanks!</div>
+		<div id="content">
+			<h1>All Done!  Thanks!</h1>
+			<p>We really appreciate your help.  If you thought this was interesting please share the study with your friends!</p>
+			<p>Study URL: <strong> http://study.truthgoggl.es</strong></p>
+			<p>If you would like to learn more about Truth Goggles <a href="http://www.truthgoggl.es">please subscribe for updates</a>.</p>
+		</div>
 	</body>
 </html>
