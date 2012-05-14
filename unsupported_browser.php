ee796d968062f44d98c42ccaefedd1407e3d6935
Daniel Schultz <slifty@gmail.com>
2012-05-14 16:38:46 -0400

    browser warning improvement

--- unsupported_browser.php
+++ unsupported_browser.php
@@ -7,6 +7,7 @@
 		<div id="content">
 			<h1>Oh no!</h1>
 			<p>Unfortunately your browser isn't currently supported by Truth Goggles.  We would love for you to participate in the study, but at this point we only support Safari, Firefox, and Chrome.  Please forgive us.</p>
+			<p>It's also entirely possible that you are viewing this through a proxy, so try clicking <a href="http://study.truthgoggl.es">this link</a> to see if it turns out any better.</p>
 		</div>
 	</body>
 </html>
