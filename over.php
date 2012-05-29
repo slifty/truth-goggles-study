ecb516be8b422974cd28e538efad24d4b8b220a7
Daniel Schultz <slifty@gmail.com>
2012-05-29 10:59:25 -0400

    fixed a typo

--- over.php
+++ over.php
@@ -8,7 +8,7 @@
 			<h1>Study Concluded</h1>
 			<p>Thank you for your interest in the Truth Goggles user study!  The study has concluded.</p>
 			<p>If you are interested in Truth Goggles please sign up to get updates at the <a href="http://truthgoggl.es">Truth Goggles home page</a>.</p>
-			<p>If you just want to Truth Goggles go check out <a href="http://truthgoggl.es/demo.html">the demo</a>.</p>
+			<p>If you just want to try Truth Goggles go check out <a href="http://truthgoggl.es/demo.html">the demo</a>.</p>
 		</div>
 	</body>
 </html>
